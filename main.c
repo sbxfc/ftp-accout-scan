c777f76977bf171468913cad4a858cc7a00e1215
truthbeauty <303040805@qq.com>
2016-09-02 16:23:25 +0800

    Commit.

--- main.c
+++ main.c
@@ -1,6 +1,3 @@
-/**
- * 2016-05-02
-*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -43,7 +40,7 @@ char* verified_accounts[ACCOUNT_COUNT*2];
 /**
  * 解析ftp协议返回的状态码
 */
-void 
+void
 get_response_code(char* buf,char* code)
 {
 	memcpy(code,buf,3);
@@ -53,15 +50,15 @@ get_response_code(char* buf,char* code)
 /**
  * 读取账号文件
  */
-void 
+void
 thread_load_acc(){
-	
+
 	FILE *fp;
 	if((fp = fopen("admin.txt","r")) == NULL){
 		printf("open error!\n");
 		exit(1);
 	}
-	
+
 	ftp_accounts_count = 0;
 	char account[MAX_TEXT_LEN];
 	while(fgets(account,MAX_TEXT_LEN,fp) !=  NULL){
@@ -80,14 +77,14 @@ thread_load_acc(){
 /**
  * 读取密码文件
  */
-void 
+void
 thread_load_pwds (){
 	FILE *fp;
 	if((fp = fopen("pwd.txt","r")) == NULL){
 		printf("文件打开失败!\n");
 		exit(1);
 	}
-	
+
 	ftp_pwds_count = 0;
 	char pwd[MAX_TEXT_LEN];
 	while(fgets(pwd,MAX_TEXT_LEN,fp) !=  NULL){
@@ -106,11 +103,10 @@ thread_load_pwds (){
 /**
  * 验证账号
 */
-int 
-verify_account (int sockfd,char* account){
-	
-	printf("验证账号->%s",account);			
-	
+int verify_account (int sockfd,char* account){
+
+	printf("验证账号->%s",account);
+
 	ssize_t n;
 	char read_buf[MAXLINE];
 	char write_buf[MAXLINE];
@@ -119,8 +115,8 @@ verify_account (int sockfd,char* account){
 	strcpy(write_buf,"USER ");
 	strcat(write_buf,account);
 	memcpy(write_buf+(strlen(write_buf)-1),"\r\n",3);
-	write(sockfd, write_buf, strlen(write_buf));	
-	memset(read_buf,0,MAXLINE);	
+	write(sockfd, write_buf, strlen(write_buf));
+	memset(read_buf,0,MAXLINE);
 	n =	read(sockfd, read_buf, MAXLINE);
 	get_response_code(read_buf,state_code);
 	if(strcmp(state_code,STATE_CODE_ACCOUNT_OK) != 0){
@@ -132,20 +128,20 @@ verify_account (int sockfd,char* account){
 }
 
 /**
- * 验证密码 
+ * 验证密码
 */
-int 
+int
 verify_pwd (int sockfd,char* pwd){
-	printf("验证密码->%s",pwd);		
-	
-	ssize_t n;	
+	printf("验证密码->%s",pwd);
+
+	ssize_t n;
 	char read_buf[MAXLINE];
-	char write_buf[MAXLINE];	
+	char write_buf[MAXLINE];
 	char*state_code = malloc(sizeof(char)*4);
 	memset(write_buf,0,MAXLINE);
 	strcpy(write_buf,"PASS ");
 	strcat(write_buf,pwd);
-	memcpy(write_buf+(strlen(write_buf)-1),"\r\n",3);	
+	memcpy(write_buf+(strlen(write_buf)-1),"\r\n",3);
 	write(sockfd, write_buf, strlen(write_buf));
 	memset(read_buf,0,MAXLINE);
 	n =	read(sockfd, read_buf, MAXLINE);
@@ -155,7 +151,7 @@ verify_pwd (int sockfd,char* pwd){
 		free(state_code);
 		return STATE_CODE_OK;
 	}
-	
+
 	/*需要重新验证账号*/
 	if(strcmp(state_code,STATE_CODE_LOGIN_USER_FIRST) == 0){
 		free(state_code);
@@ -173,7 +169,7 @@ verify_pwd (int sockfd,char* pwd){
 */
 int idx = 0;
 void save_account (char* account,char* pwd){
-		
+
 	int len = strlen(account);
 	char* p = malloc(sizeof(char)*(len+1));
 	memcpy(p,account,len);
@@ -197,7 +193,7 @@ void save_account (char* account,char* pwd){
  * @param account 验证的账号
 */
 int scan_pwd(char* ip,int port,int* account){
-	
+
 	char read_buf[MAXLINE];
 	char write_buf[MAXLINE];
 
@@ -217,12 +213,12 @@ int scan_pwd(char* ip,int port,int* account){
 		 printf("connecting error!%i\n",rst);
 		 exit(-1);
 	}
-	
+
 	memset(read_buf,0,MAXLINE);
 	n = read(sockfd, read_buf, MAXLINE);
 	get_response_code(read_buf,state_code);
 	if(strcmp(state_code,STATE_CODE_CONNECT_SUCC) != 0){
-		printf("ftp->%s\r",read_buf);				
+		printf("ftp->%s\r",read_buf);
 		exit(-1);
 	}
 	printf("ftp--->%s\r",read_buf);
@@ -230,7 +226,7 @@ int scan_pwd(char* ip,int port,int* account){
 
 	/*验证账号*/
 	if(strlen(account)>0){
-		printf("验证账号-->:%s",account);				
+		printf("验证账号-->:%s",account);
 		if(verify_account(sockfd,account) == STATE_CODE_FAILED){
 			return STATE_CODE_FAILED;
 		}
@@ -249,7 +245,7 @@ int scan_pwd(char* ip,int port,int* account){
 					printf("-->验证成功,密码为 %s\n",pwd);
 					save_account(account,pwd);
 					free(state_code);
-					return STATE_CODE_OK;																
+					return STATE_CODE_OK;
 					break;
 				case STATE_CODE_REVALIDATE:
 					/*需要重新验证账号*/
@@ -258,7 +254,7 @@ int scan_pwd(char* ip,int port,int* account){
 							printf("-->验证成功,密码为 %s\n",pwd);
 							save_account(account,pwd);
 							free(state_code);
-							return STATE_CODE_OK;									
+							return STATE_CODE_OK;
 						}
 					}
 					break;
@@ -268,8 +264,8 @@ int scan_pwd(char* ip,int port,int* account){
 			}
 		}
 	}
-	
-	
+
+
 	free(state_code);
 	return STATE_CODE_FAILED;
 }
@@ -292,12 +288,12 @@ void thread_run(void *arg)
  * 开启多线程验证
 */
 void multi_thread_scan(int* ip,int port,int thread_count){
-	
+
 	pthread_t *thread;
     thread = (pthread_t*)malloc(sizeof(pthread_t)*thread_count);
-	
+
 	for (int i = 0; i < thread_count;i++){
-		
+
 		struct argument *arg;
 
 		int offset = (ftp_accounts_count / thread_count)*i;
@@ -311,7 +307,7 @@ void multi_thread_scan(int* ip,int port,int thread_count){
 		pthread_create(&thread[i], NULL, thread_run,(void *)arg);
 	}
 
-    
+
     for (int j = 0; j < thread_count; j++) {
         void *thread_return;
         int ret = pthread_join(thread[j],&thread_return);/*等待第一个线程退出，并接收它的返回值*/
@@ -325,7 +321,7 @@ void multi_thread_scan(int* ip,int port,int thread_count){
 }
 
 
-int 
+int
 main(int argc,char ** argv)
 {
 
@@ -338,21 +334,21 @@ main(int argc,char ** argv)
 	clock_t start_time;
 	clock_t finish_time;
 	int duration;
-	
+
 	/*ftp主机信息*/
 	char* ip = argv[1];
 	int port = atoi(argv[2]);
 	ssize_t n;
-	
+
 	start_time = time(NULL);
-	
+
 	/*加载账号列表*/
 	pthread_t tid_read_acc;
 	if(pthread_create(&tid_read_acc,NULL,thread_load_acc,NULL)!=0){
 		printf("error creating thread!");
 		exit(-1);
 	}
-	
+
 	/*加载密码列表*/
 	pthread_t tid_read_pwd;
 	if(pthread_create(&tid_read_pwd,NULL,thread_load_pwds,NULL)!=0){
@@ -368,7 +364,7 @@ main(int argc,char ** argv)
     if (pthread_join(tid_read_pwd, NULL) != 0){
 		exit(-1);
     }
-	
+
 	/*开启多线程扫描账号*/
 	multi_thread_scan(ip,port,SCAN_THREAD_COUNT);
 
@@ -384,10 +380,10 @@ main(int argc,char ** argv)
     printf("             ||     ||\n\n");
 	printf("-->扫描结束.\n");
 
-	
+
 	finish_time = time(NULL);
-	duration = (finish_time - start_time); 
-	printf( "-->花费时间%d秒!\n", duration ); 
+	duration = (finish_time - start_time);
+	printf( "-->花费时间%d秒!\n", duration );
 
 	for(int i = 0; i < ACCOUNT_COUNT;i++){
 		int idx = 2*i;
@@ -399,6 +395,6 @@ main(int argc,char ** argv)
 			printf("-->密码:%s",verified_accounts[idx+1]);
 		}
 	}
-	
+
 	return 0;
 }
